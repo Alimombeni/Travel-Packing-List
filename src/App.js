47c55c4c4ce1b184088910f773e19e4c98cde473
alimombeni <ali.mombenni@gmail.com>
2024-06-24 12:15:23 +0330

    [ADD:input and select element add to form]

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import PackingList from "./PackingList";
 import Stats from "./Stats";
 
 
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -17,6 +18,7 @@ function App() {
 return(
     <div className='app'>
         <Logo/>
+
         <Form/>
         <PackingList dataItem={initialItems}/>
         <Stats/>
--- src/Form.js
+++ src/Form.js
@@ -2,25 +2,30 @@ import {useState} from "react";
 
 function Form (){
 
-
+    const [description , setDescription] =useState('')
+    const [quantity,setQuantity]=useState()
 function handleSubmit (e) {
     e.preventDefault()
 
-
+if (!description) return;
+    const newItem = {description,quantity,packed: false ,id:new Date()}
+    console.log(newItem)
+    setQuantity(1)
+    setDescription('')
 }
 
     return(
         <form className='add-form' onSubmit={handleSubmit}>
 
             <h3>What do you need for your trip?</h3>
-            <select>
+            <select value={quantity} onChange={(e)=>setQuantity(e.target.value)}>
 
                 {Array.from({length: 20} , (_, i) => i + 1 ).
                 map((num)=>( <option value={num} key={num}>{num}</option>))}
 
 
             </select>
-            <input type="text" placeholder="item..."/>
+            <input type="text" placeholder="item..." value={description} onChange={(e)=>setDescription(e.target.value)}/>
             <button>Add</button>
         </form>
     )
